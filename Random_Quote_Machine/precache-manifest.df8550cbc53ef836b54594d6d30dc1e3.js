4c94ee0d84c920cf10d93d03108605b961bf87d8
prokawsar <prokawsar@gmail.com>
2019-07-25 16:45:32 +0600

    change folder name, link first app

---
+++ Random_Quote_Machine/precache-manifest.df8550cbc53ef836b54594d6d30dc1e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce9fd78860d772cd74e8",
+    "url": "/static/js/main.ce9fd788.chunk.js"
+  },
+  {
+    "revision": "1360180a8244fa7f7158",
+    "url": "/static/js/1.1360180a.chunk.js"
+  },
+  {
+    "revision": "ce9fd78860d772cd74e8",
+    "url": "/static/css/main.af94dc73.chunk.css"
+  },
+  {
+    "revision": "3d5143e0ae041450b25f449fcb4c9eb5",
+    "url": "/index.html"
+  }
+];
