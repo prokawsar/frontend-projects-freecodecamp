679ca5de6fed8f48c2a42f4d51c669a2e5b4b47c
prokawsar <prokawsar@gmail.com>
2019-10-29 21:24:04 +0600

    icon added

--- Pomodoro_Clock/src/components/TimeSetter.js
+++ Pomodoro_Clock/src/components/TimeSetter.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper} from '@material-ui/core';
+import {Grid, Paper, Typography, Icon } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,13 +16,29 @@ const useStyles = makeStyles(theme => ({
 
 
 const TimeSetter = (props) => {
-
+  const [minute, setMinute] = useState(
+    5
+  );
+  let second = 0;
   const classes = useStyles();
   const  {text, size} = props.conf;
 
+  const handleIncrement = () => {
+    setMinute(currentMinute => currentMinute < 60 ? currentMinute + 1 : currentMinute)
+  }
+  const handleDecrement = () => {
+    setMinute(currentMinute => currentMinute > 0 ? currentMinute - 1 : currentMinute)
+  }
+
   return (
     <Grid item xs={size} >
-      <Paper className={classes.paper}>{text}</Paper>
+      <Paper className={classes.paper}>{text}
+        <Typography variant="h1" > {minute}:{second}</Typography>
+
+        <Icon style={{ fontSize: 50, cursor: 'pointer' }} onClick={handleDecrement}>arrow_drop_down</Icon>
+        <Icon style={{ fontSize: 50, cursor: 'pointer'}} onClick={handleIncrement}>arrow_drop_up</Icon>
+
+      </Paper>
     </Grid>
   );
 
