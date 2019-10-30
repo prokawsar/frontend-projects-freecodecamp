6c9dfa05f760088aa8d383a816c4513236231312
prokawsar <prokawsar@gmail.com>
2019-10-30 13:55:42 +0600

    live for pomodoro

---
+++ Pomodoro_Clock/precache-manifest.64e7ec1d5bfdb10b9430e43899751d9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae18029cf9c0a501793c88274dfdf32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddeb1de2362901d7b621",
+    "url": "/static/js/2.49b865df.chunk.js"
+  },
+  {
+    "revision": "ffd29e403e7b8225e172",
+    "url": "/static/js/main.15efded0.chunk.js"
+  },
+  {
+    "revision": "f2dd5f5522e35ae681a2",
+    "url": "/static/js/runtime-main.904d84f5.js"
+  }
+]);
